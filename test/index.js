8a02e51e92444f384874d9589b91d48c38568638
gyson <eilian.yunsong@gmail.com>
2014-11-02 01:03:25 +0800

    rename test

--- test/index.js
+++ test/index.js
@@ -5,29 +5,37 @@ var assert = require('assert')
 describe('thunk()', function () {
     describe('sync callback', function () {
         it('should use result from first callback', function (done) {
+            var isSync = true
             thunk(function (cb) {
                 cb('ERROR_1')
                 cb('ERROR_2')
                 cb('ERROR_3')
             })(function (err) {
+                assert(isSync)
                 assert(arguments.length === 1)
                 assert(err === 'ERROR_1')
                 done()
             })
+            isSync = false
         })
-        it('should catch exception if any', function (done) {
-            thunk(function (cb) {
-                throw 'ERROR'
-            })(function (err) {
-                assert(arguments.length === 1)
-                assert(err === 'ERROR')
-                done()
-            })
+        it('should not catch exception if any', function (done) {
+            var match = false
+            var err = new Error()
+            try {
+                thunk(function (cb) {
+                    throw err
+                })
+            } catch (e) {
+                match = e === err
+            }
+            assert(match)
+            done()
         })
         it('should return same result for multiple calls', function (done) {
             var th = thunk(function (cb) {
                 cb(null, 'VALUE')
             })
+            var isSync = true
             th(function (err, val) {
                 assert(err === null)
                 assert(val === 'VALUE')
@@ -35,17 +43,21 @@ describe('thunk()', function () {
                     assert(err === null)
                     assert(val === 'VALUE')
                     th(function (err, val) {
+                        assert(isSync)
                         assert(err === null)
                         assert(val === 'VALUE')
                         done()
                     })
                 })
             })
+            isSync = false
         })
         it('should accept multipe arguments', function (done) {
+            var isSync = true
             thunk(function (cb) {
                 cb('A_0', 'A_1', 'A_2', 'A_3')
             })(function () {
+                assert(isSync)
                 assert(arguments.length === 4)
                 assert(arguments[0] === 'A_0')
                 assert(arguments[1] === 'A_1')
@@ -53,12 +65,15 @@ describe('thunk()', function () {
                 assert(arguments[3] === 'A_3')
                 done()
             })
+            isSync = false
         })
         it('should reserve the context of callback', function (done) {
             var ctx = {}
+            var isSync = true
             thunk(function (cb) {
                 cb.call(ctx, 0, 1, 2, 3, 4)
             })(function () {
+                assert(isSync)
                 assert(arguments.length === 5)
                 assert(this === ctx)
                 assert(arguments[0] === 0)
@@ -68,6 +83,7 @@ describe('thunk()', function () {
                 assert(arguments[4] === 4)
                 done()
             })
+            isSync = false
         })
     })
     describe('async callback', function () {
